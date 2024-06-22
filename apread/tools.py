4d429e5ac7dfe94a6d14f1b8129073bc87451db6
Leon Bohmann <leon.bohmann@mepla.net>
2024-06-22 15:40:57 +0200

    add utils

--- apread/tools.py
+++ apread/tools.py
@@ -1,3 +1,4 @@
+from typing import Any
 import warnings
 import functools
 
@@ -14,3 +15,73 @@ def deprecated(func):
         warnings.simplefilter('default', DeprecationWarning)  # reset filter
         return func(*args, **kwargs)
     return new_func
+
+def untilSeconds(chan, seconds):
+    """
+    Get data from a channel from the start for a certain amount of seconds.
+    """
+    if chan.isTime and seconds is not None:
+        d = chan.data[chan.data < chan.data[0] + seconds]
+        return d,d
+
+    if seconds is not None:
+        time = chan.Time.data
+        data = chan.data
+        time = time[time < time[0] + seconds]
+        if len(time) == 0:
+            print(f"untilSeconds: No data until {seconds} in channel {chan.name}.")
+        data = data[:len(time)]
+        return time, data
+    return chan.Time.data, chan.data
+
+def afterSeconds(chan: Any | tuple, seconds):
+    """
+    Get data from a channel from the end for a certain amount of seconds.
+    """
+    if isinstance(chan, tuple):
+        time = chan[0]
+        data = chan[1]
+        name = ""
+    else:
+        time = chan.Time.data
+        data = chan.data
+        name = chan.name
+        if chan.isTime and seconds is not None:
+            d = chan.data[chan.data > seconds]
+            return d,d
+        
+    if seconds is not None:
+        time = time[time > seconds]
+        if len(time) == 0:
+            print(f"afterSeconds: No data after {seconds} in channel {name}.")
+        data = data[-len(time):]
+        return time, data
+
+def betweenSeconds(chan, start, end):
+    if isinstance(chan, tuple):
+        time = chan[0]
+        data = chan[1]
+    else:
+        time = chan.Time.data
+        data = chan.data
+
+        if chan.isTime and start is not None and end is not None:
+            d = chan.data[(chan.data > chan.data[0] + start) & (chan.data < chan.data[0] + end)]
+            return d, d
+        elif start is None or end is None:
+            return chan.Time.data, chan.data
+
+    
+    if start is not None and end is not None:
+        mask = (time > start) & (time <  end)
+
+        time = time[mask]
+
+        if len(time) == 0:
+            print(f"betweenSeconds: No data between {start} and {end} in channel {chan.name}.")
+        data = data[mask]
+    
+    return time, data
+    
+
+
